2fcbeb5ee0c2fc630dd1eb3db28b1f833d6988b6
azmikamis <azmi.stuff@gmail.com>
2015-04-23 04:47:06 +0800

    templates and settings

--- djangoplayground/settings.py
+++ djangoplayground/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'djangoplayground.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -94,7 +96,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
---
+++ djangoplayground/views.py
@@ -0,0 +1,9 @@
+__author__ = 'azmi'
+
+from django.shortcuts import render
+
+
+def home(request):
+    context = {}
+    template = "home.html"
+    return render(request, template, context)
